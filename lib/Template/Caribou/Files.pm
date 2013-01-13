6edf1a42efdd1c9e0c90c8f001b1d8041f46943f
Yanick Champoux <yanick@babyl.dyndns.org>
2013-01-13 18:54:56 -0500

    documentation

--- lib/Template/Caribou/Files.pm
+++ lib/Template/Caribou/Files.pm
@@ -1,4 +1,49 @@
 package Template::Caribou::Files;
+#ABSTRACT: Role to load templates from files
+
+=head1 SYNOPSIS
+
+    package MyTemplate;
+
+    use Moose;
+
+    with 'Template::Caribou';
+    with 'Template::Caribou::Files' => {
+        dirs => [ 'my_templates/' ],
+        auto_reload => 1,
+    };
+
+    1;
+
+=head1 DESCRIPTION
+
+With I<Template::Caribou::Files>, a Caribou class will automatically import
+all template file (i.e., all files with a C<.bou> extension) under the given directories.
+
+The names of the imported templates will be their path, relative to the
+imported directories, without their extension. To take the example in the
+L</SYNOPSIS>, if the content of C<my_templates/> is:
+
+    ./foo.bou
+    ./bar.bou
+    ./baz/frob.bou
+
+then the templates C<foo.bou>, C<bar.bou> and C<baz/frob> will be created.
+
+=head1 ROLE PARAMETERS
+
+=head2 dirs
+
+The array ref of directories to scan for templates. 
+
+=head2 auto_reload
+
+If set to true, the import directories will be rescanned and every file
+re-imported before every call to C<render>. Useful during development.
+
+Defaults to false.
+
+=cut
 
 use strict;
 use warnings;
