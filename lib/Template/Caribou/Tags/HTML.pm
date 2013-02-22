70ab626bafb34137b0b7b13ea7f3fb75279f1b71
Yanick Champoux <yanick@babyl.dyndns.org>
2013-02-22 11:56:36 -0500

    add table tags

--- lib/Template/Caribou/Tags/HTML.pm
+++ lib/Template/Caribou/Tags/HTML.pm
@@ -10,13 +10,16 @@ BEGIN {
         p html head h1 h2 h3 h4 h5 h6 body emphasis div
         style title span li ol ul i b bold a form input
         label link img section article
+        table thead tbody table_row th td
     /;
 }
 
 use Template::Caribou::Tags
     'render_tag',
     'attr',
+    mytag => { -as => 'table_row', name => 'tr' },
     map { ( mytag => { -as => $_, name => $_ } ) }
+        grep { !/table_row/ }
         @Template::Caribou::Tags::HTML::TAGS;
 
 use Sub::Exporter -setup => {
