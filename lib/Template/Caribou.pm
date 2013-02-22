0d8b272444e967f626b4c44e7fbec5fcdf6a06ff
Yanick Champoux <yanick@babyl.dyndns.org>
2013-02-22 15:04:18 -0500

    add manual

--- lib/Template/Caribou.pm
+++ lib/Template/Caribou.pm
@@ -1,15 +1,16 @@
 package Template::Caribou;
-# ABSTRACT: class-based *ML-centric templating system
+# ABSTRACT: class-based HTML-centric templating system
 
 =head1 SYNOPSIS
 
     package MyTemplate;
 
     use Moose;
+    use Template::Caribou;
+
     with 'Template::Caribou';
 
-    use Template::Caribou::Utils;
-    use Template::Caribou::Tags::HTML;
+    use Template::Caribou::Tags::HTML qw/ :all /;
 
     has name => ( is => 'ro' );
 
@@ -30,7 +31,7 @@ package Template::Caribou;
 
     package main;
 
-    my $template = MyTemplate->new( name => 'Bob' );
+    my $template = MyTemplate->new( name => 'Yanick' );
     print $template->render('page');
 
 =head1 DESCRIPTION
@@ -39,8 +40,11 @@ WARNING: Codebase is alpha with extreme prejudice. Assume that bugs are
 teeming and that the API is subject to change.
 
 L<Template::Caribou> is a L<Moose>-based, class-centric templating system
-mostly aimed at producing sgml-like outputs (HTML, XML, SVG, etc). It is
+mostly aimed at producing sgml-like outputs, mostly HTML, but also XML, SVG, etc. It is
 heavily inspired by L<Template::Declare>.
+
+For a manual on how to use C<Template::Caribou>, have a peek at
+L<Template::Caribou::Manual>.
 
 =cut
 
